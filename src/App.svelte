69f1df0643faba95db3a79bf192222521396a082
satyam <satyam@technoculture.io>
2022-03-11 19:57:52 +0530

    style: minor update on bg color

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,10 @@
 <main>
   <div class="w-screen h-screen max-h-[480px] max-w-[800px] bg-gray-100">
     <div class="grid gap-6 grid-cols-6 grid-rows-5 w-full h-full p-10">
-      <div class="row-span-4 col-span-2 rounded-2xl bg-slate-500 shadow-2xl">
+      <div class="row-span-4 col-span-2 rounded-2xl bg-black/50 shadow-xl backdrop-blur-sm ring-1 ring-slate-200">
         <img src={naked} alt="" class="pt-12" />
       </div>
-      <div class="row-span-2 bg-slate-600 rounded-xl shadow-2xl flex flex-col">
+      <div class="row-span-2 bg-black/50 rounded-xl shadow-xl flex flex-col backdrop-blur-sm">
         <span class="text-sm p-3 text-white text-thin"> Wafer Mask Distance</span>
         <div class="grow w-full flex place-content-center bg-black rounded-xl ring-1 ring-white shadow-xl">
           <span class="text-orange-600 text-xl place-self-center justify-center">2.8um</span>
@@ -22,13 +22,13 @@
       <div class="row-span-2"> Light Intensity </div>
       <div class="row-span-2"></div>
       <div class="row-span-2"></div>
-      <div class="bg-slate-800 rounded-xl flex"><Icon src="{InformationCircle}" class="h-12 text-gray-400 place-self-center"/></div>
-      <div class="bg-red-300 rounded-xl">
+      <div class="rounded-xl flex bg-black/50 shadow-xl backdrop-blur-sm ring-1 ring-slate-200"><Icon src="{InformationCircle}" class="h-12 text-white place-self-center"/></div>
+      <div class="bg-black/50 shadow-xl backdrop-blur-sm ring-1 ring-slate-200 rounded-xl">
         <div class="bg-green-400 w-2 inset-0" />
       </div>
       <div>4S</div>
       <div>365nm</div>
-      <div class="col-span-2 flex bg-slate-800 text-gray-400 rounded-xl place-content-center">
+      <div class="col-span-2 flex bg-black/50 shadow-xl backdrop-blur-sm ring-1 ring-slate-200 text-white rounded-xl place-content-center">
         <span class="place-self-center text-2xl mr-4">00:00:00</span>
         <Icon src="{Play}" class="place-self-center h-12 w-12" />
       </div>
