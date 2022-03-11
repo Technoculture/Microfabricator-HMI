7b5f9b21870a26ce7c170ecb275541bbb06b994d
satyam <satyam@technoculture.io>
2022-03-11 15:17:14 +0530

    style: 800x480 div

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,19 @@
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" class="h-96" />
+  <div class="h-[480px] w-[800px] overflow-hidden">
+    <img src={logo} alt="Svelte Logo" class="h-96" />
 
-  <Counter />
+    <div class="flex flex-col">
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
 
-  <p>
-    Visit <a href="http://www.technoculture.io">technoculture.io</a> to learn more.
-  </p>
+    <p>
+      Visit <a href="http://www.technoculture.io">technoculture.io</a> to learn more.
+    </p>
+  </div>
 </main>
 
 <style>
