6d904a91f6b0e7bf27154d0927247219a41e35b8
satyam <satyam@technoculture.io>
2022-03-15 23:31:34 +0530

    style: add dialogs for ejecting modules

--- apps/hmi/src/lib/screens/Base.svelte
+++ apps/hmi/src/lib/screens/Base.svelte
@@ -1,26 +1,37 @@
 <script lang="ts">
-// import { writable } from "svelte/store";
-
   import Card from "../Card.svelte";
-import LockOpen from "../Icons/LockOpen.svelte";
+  import LockOpen from "../Icons/LockOpen.svelte";
+  import RightArrow from "../Icons/RightArrow.svelte";
+  import UpArrow from "../Icons/UpArrow.svelte";
 
   let stageButtonActive = false;
-  const toggleStageButton = () => {
-    bind:stageButtonActive = !stageButtonActive;
+  function toggleStageButton() {
+    stageButtonActive = !stageButtonActive;
+  }
+
+  let leButtonActive = false;
+  function toggleLeButton() {
+    leButtonActive = !leButtonActive;
   }
 </script>
 
 <div class="flex snap-center overflow-x-scroll scrollbar-hide snap-x snap-mandatory h-full">
   <div class="flex flex-col">
     <slot />
-    {#if stageButtonActive}
+    {#if stageButtonActive || leButtonActive}
       <div class="absolute bottom-0 mb-14 w-full p-4 bg-black flex flex-col">
         <span class="text-white py-1 text-xs">
-          Are you sure, this action will unlock the stage module 4S and allow you to remove it out
+          {#if stageButtonActive}
+            Are you sure, this action will unlock the stage module 4S and allow you to remove it out
+          {:else if leButtonActive}
+            Are you sure, this action will unlock the light engine module and allow you to remove it out
+          {/if}
         </span>
-        <button class="font-bold px-2 h-12 bg-red-500 my-1 rounded-lg grow flex place-content-between">
+        <button class="px-6 h-10 bg-red-500 active:bg-red-600 my-1 rounded-lg grow flex place-content-between"
+          on:click={toggleStageButton}
+        >
           <span class="place-self-center">Eject</span>
-          <LockOpen class="place-self-center h-4" />
+          <LockOpen class="place-self-center h-5 w-5" />
         </button>
       </div>
     {/if}
@@ -30,15 +41,27 @@ import LockOpen from "../Icons/LockOpen.svelte";
 <div class="flex h-16">
   <Card class="col-span-2 flex text-white text-center font-extralight text-xl overflow-hidden grow rounded-none" hasRing={true}>
     <div class="flex flex-grow h-full active:bg-black ">
-      <span class="flex-grow place-self-center" 
+      <span class="grow place-content-center flex" 
         on:click={toggleStageButton}
       >
-        4S
+        <span class="place-self-center pr-4">4S</span>
+        {#if stageButtonActive}
+          <UpArrow class="h-4 w-4 place-self-center" />
+        {:else}
+          <RightArrow class="h-4 w-4 place-self-center" />
+        {/if}
       </span>
     </div>
     <div class="flex flex-grow h-full active:bg-black" >
-      <span class="flex-grow place-self-center">
-        365nm
+      <span class="grow place-content-center flex"
+        on:click={toggleLeButton}
+      >
+        <span class="place-self-center pr-4">365nm</span>
+        {#if leButtonActive}
+          <UpArrow class="h-4 w-4 place-self-center" />
+        {:else}
+          <RightArrow class="h-4 w-4 place-self-center" />
+        {/if}
       </span>
     </div>
   </Card>
