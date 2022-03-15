4b9bb70bc48711447554b20e4a49bc3f63052299
satyam <satyam@technoculture.io>
2022-03-16 00:04:12 +0530

    style: page indicator

--- apps/hmi/src/lib/screens/Home.svelte
+++ apps/hmi/src/lib/screens/Home.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import naked from "../../assets/mfab-naked.png";
+  import slider2 from "../../assets/slider.png";
+  import lefront from "../../assets/le-front.png";
   import Base from "./Base.svelte";
 </script>
 
 <Base>
-  <div class="flex snap-center overflow-x-scroll scrollbar-hide snap-x snap-mandatory">
+  <div class="flex snap-center overflow-x-scroll scrollbar-hide snap-x snap-mandatory h-full">
     <img src={naked} alt="" class="snap-center object-cover" />
-    <img src={naked} alt="" class="snap-center object-cover" />  
-    <img src={naked} alt="" class="snap-center object-cover" />
+    <img src={slider2} alt="" class="snap-center object-cover pb-2" />  
+    <img src={lefront} alt="" class="snap-center object-cover p-2" />  
+  </div>
+  <div class="absolute flex mt-52 p-4 w-full place-content-center font-thin">
+    <span class="text-5xl text-slate-300 place-self-center">-</span>
+    <span class="text-5xl text-slate-700 place-self-start">-</span>
+    <span class="text-5xl text-slate-700 place-self-start">-</span>
   </div>
 </Base>
