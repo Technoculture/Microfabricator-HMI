1eee81a73274bf760ebd9492497b43747c3596e1
satyam <satyam@technoculture.io>
2022-03-19 11:00:24 +0530

    refactor: logger configuration

---
+++ apps/comm-server/src/logger.ts
@@ -0,0 +1,30 @@
+const winston = require('winston');
+
+const appLogger = winston.createLogger({
+  level: 'verbose',
+  defaultMeta: { service: 'mfab-hardware-service' },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.prettyPrint()
+  ),
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `mfab-error.log`
+    // - Write all logs with importance level of `info` or less to `mfab.log`
+    //
+    new winston.transports.File({ filename: 'mfab-error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'mfab.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format
+//
+if (process.env.NODE_ENV !== 'production') {
+  appLogger.add(new winston.transports.Console({
+    level: 'silly',
+    format: winston.format.simple()
+  }));
+}
+
+module.exports = appLogger;
